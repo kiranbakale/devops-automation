2c36f97c19ebe0cace335f3f043e2b0a7da75e04
Kiran Bakale <46279617+kiranbakale@users.noreply.github.com>
2024-04-08 15:05:42 +0530

    changed

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "Hello from spinnaker-uat-app";
 	}
 
 	public static void main(String[] args) {
